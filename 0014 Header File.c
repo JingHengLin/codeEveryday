1458748c31a8ee6138fbc809b109319054e99504
JingHengLin <405500538@gms.tku.edu.tw>
2020-12-28 16:43:29 +0800

    Header File

---
+++ 0014 Header File.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "0014 Header File.h"
+
+int main()
+{
+    int ans = add(10,2);
+    printf("%s %d\n",MYNAME, ans);
+    return 0;
+}
---
+++ 0014 Header File.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MYNAME "abc"
+int add(int a, int b);
+int add(int a,int b)
+{
+    return a+b;
+}
